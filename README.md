962e0da1308202707f29508d2d3fa3b0bc4bbe6a
sursev07 <simangunsong.seven@gmail.com>
2021-04-08 23:54:35 +0700

    Update component

--- README.md
+++ README.md
@@ -14,6 +14,6 @@ import H8Footer from 'v-h8footer'
 Vue.use(H8Footer)
 
 ## If not globally, you can also import the individual components locally.
-import 'H8Footer' from 'v-h8footer'
+import H8Footer from 'v-h8footer'
 
 ```
--- src/install.js
+++ src/install.js
@@ -4,7 +4,7 @@ const H8FooterSimple = {
   install (Vue, options) {
     // Let's register our component globally
     // https://vuejs.org/v2/guide/components-registration.html
-    Vue.component('h8-footer', H8Footer)
+    Vue.component('H8Footer', H8Footer)
   }
 }
 
